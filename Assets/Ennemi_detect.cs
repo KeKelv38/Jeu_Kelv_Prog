5937d14d27e405148ea80c98eed490360bfece76
KeKelv38 <k.grimault@rns.ecolescreatives.com>
2024-02-15 16:45:56 +0100

    version15_02
    ennemi qui suit le joueur

---
+++ Assets/Ennemi_detect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemi_detect : MonoBehaviour
+{
+    public bool isChasing;
+    public Transform playerTransform;
+    public float chaseDistance;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (isChasing)
+        {
+            if(transform.position.x > playerTransform.position.x)
+            {
+                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            }
+            if (transform.position.x < playerTransform.position.x)
+            {
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if(Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+            {
+                isChasing = true;
+            }
+        }
+    }
+}
